5b72487e734f34d598bc580be0246dd69b495350
Antoine Boulanger <antoine@locomotive.ca>
2020-02-12 15:36:53 -0500

    Update hasTouch support condition

--- src/support.js
+++ src/support.js
@@ -4,7 +4,7 @@ module.exports = (function getSupport() {
     return {
         hasWheelEvent: 'onwheel' in document,
         hasMouseWheelEvent: 'onmousewheel' in document,
-        hasTouch: 'ontouchstart' in document,
+        hasTouch: ('ontouchstart' in window) || window.TouchEvent || window.DocumentTouch && document instanceof DocumentTouch,
         hasTouchWin: navigator.msMaxTouchPoints && navigator.msMaxTouchPoints > 1,
         hasPointer: !!window.navigator.msPointerEnabled,
         hasKeyDown: 'onkeydown' in document,
